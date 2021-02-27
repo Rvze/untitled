d758be18592517a85f28c7ea74a87be24264146d
Rvze <makarovzhest@mail.ru>
2021-02-27 14:36:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Set<MyClass> set = new HashSet<>();
+        System.out.println("1");
+        set.add(new MyClass(11L, "name1"));
+
+        System.out.println("2");
+        set.add(new MyClass(12L, "name2"));
+
+        System.out.println("3");
+        set.add(new MyClass(13L, "name3"));
+
+        System.out.println("4");
+        set.add(new MyClass(11L, "name1"));
+        System.out.println("SIZE: "+set.size());
+    }
+
+}
